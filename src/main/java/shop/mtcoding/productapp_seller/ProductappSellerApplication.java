9f4234fcf131a3eec2f7760456a0117ceb760a64
junfminK1m <deliah07@naver.com>
2023-04-14 11:53:41 +0900

    프로젝트 초기세팅 (yml, build.gradle)

---
+++ src/main/java/shop/mtcoding/productapp_seller/ProductappSellerApplication.java
@@ -0,0 +1,13 @@
+package shop.mtcoding.productapp_seller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductappSellerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductappSellerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/shop/mtcoding/productapp_seller/ProductappSellerApplicationTests.java
@@ -0,0 +1,13 @@
+package shop.mtcoding.productapp_seller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductappSellerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
